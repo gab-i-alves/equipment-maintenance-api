ca3a02b908b32fda7e61b72c9f863790ee031c17
gab-i-alves <gab.alvesnacimento@gmail.com>
2024-11-06 22:08:00 -0300

    ajustes no UsuarioRepository para suportar a busca por emal

--- src/main/java/com/web2/projetoweb2/repositorys/UsuarioRepository.java
+++ src/main/java/com/web2/projetoweb2/repositorys/UsuarioRepository.java
@@ -3,8 +3,11 @@ package com.web2.projetoweb2.repositorys;
 import com.web2.projetoweb2.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
     Usuario findById(Long id);
+
+    Optional<Usuario> findByEmail(String email);
 }
